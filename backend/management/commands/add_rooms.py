44591270ed92cdbcabd284c709bf8bea70b226f4
Gursimran <Gursimran1@macbook-pro.home>
2024-03-17 23:14:35 +0000

    pip_requirements.txt'

--- backend/management/commands/add_rooms.py
+++ backend/management/commands/add_rooms.py
@@ -5,14 +5,15 @@ from backend.models import Room
 class Command(BaseCommand):
     help = 'Add shared rooms to the database'
 
-    def handle(self, *args, **options):
-        shared_room_numbers = [1, 2, 16, 17]
+    def add_arguments(self, parser):
+        parser.add_argument('num_rooms', type=int, help='Number of rooms to add')
 
+    def handle(self, *args, **options):
+        num_rooms = options['num_rooms']
+        
         for i in range(1,43):
-            if i in shared_room_numbers:
-                room, created = Room.objects.get_or_create(id=i, is_shared=True)
-            else:
-                room, created = Room.objects.get_or_create(id=i)
+            
+            room, created = Room.objects.get_or_create(id=i)
 
             if created:
                 self.stdout.write(self.style.SUCCESS(f"Shared room  added successfully."))
